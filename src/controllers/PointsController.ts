c00c2747833e1cdbea57e33878aa6e67764e1db7
Yago Henrique <yagohsp@unipam.edu.br>
2020-06-02 21:35:10 -0300

    backend

---
+++ src/controllers/PointsController.ts
@@ -0,0 +1,85 @@
+import knex from '../database/connection';
+import { Request, Response } from 'express'
+
+class PointsController {
+    async index(request: Request, response: Response){
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items).split(',').map(item => Number(item.trim()));
+
+        const points = await knex('points')
+        .join('point_Items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*');
+
+        return response.json(points);
+    }
+
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+
+        const point = await knex('points').where('id', id).first();
+
+        if(!point){
+            return response.status(400).json({ message: 'Point not found' });
+        }
+
+        const items = await knex('items')
+        .join('point_items', 'items.id', '=', 'point_items.item_id')
+        .where('point_items.point_id', id)
+        .select("items.title");
+
+        return response.json({point, items})
+    }
+
+    async create(request: Request, response: Response){
+        const {
+            name,
+            email,
+            whatsapp,
+            items,
+            latitude,
+            longitude,
+            city,
+            uf
+        } = request.body;
+    
+        const transaction = await knex.transaction();
+    
+        const point = {
+            image: 'image',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf
+        }
+
+        const insertedIds = await transaction('points').insert(point)
+    
+        const point_id = insertedIds[0];
+    
+        const pointItems = await items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id
+            }
+        })
+    
+        await transaction('point_items').insert(pointItems);
+    
+        await transaction.commit();
+
+        return response.json({ 
+            id: point_id,
+            ...point,
+         })
+    }
+}
+
+export default PointsController;
---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
+const routes = express.Router();
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes
+.get('/items', itemsController.index);
+
+routes
+.post('/points', pointsController.create)
+.get('/points', pointsController.index)
+.get('/points/:id', pointsController.show);
+
+
+export default routes;
